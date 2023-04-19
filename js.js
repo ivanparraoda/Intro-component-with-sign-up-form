9aa7f1d07f18ea71cc1c0c48dad43e0a9080fa8d
Ivan Parra <98499975+ivanparraoda@users.noreply.github.com>
2023-04-18 21:40:35 -0600

    commit
    queries added

---
+++ js.js
@@ -0,0 +1,9 @@
+
+function validateForm() {
+    let x = document.forms["form"]["fname"].value;
+    if (x == "") {
+        
+      alert("Name must be filled out");
+      return false;
+    }
+  }
